66cfe4f94717eb0cfa1f6e063a5134e10554bdf8
Muzammilzia <muzzammilzia20@gmail.com>
2024-11-18 23:00:33 +0100

    feat: added login & register page for candidate.

---
+++ src/pages/auth/candidate/login.tsx
@@ -0,0 +1,152 @@
+// IN USE, TEMPLATE FROM pages/auth-demo/login/modern.tsx
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import ArrowLeftIcon from "@untitled-ui/icons-react/build/esm/ArrowLeft";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import SvgIcon from "@mui/material/SvgIcon";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+
+import { RouterLink } from "src/components/router-link";
+import { Seo } from "src/components/seo";
+import { paths } from "src/paths";
+import { AuthContextType } from "src/contexts/auth/jwt-context";
+import { useAuth } from "src/hooks/use-auth";
+import { useMounted } from "src/hooks/use-mounted";
+import { useRouter } from "src/hooks/use-router";
+import { useSearchParams } from "src/hooks/use-search-params";
+import { FormHelperText } from "@mui/material";
+
+interface Values {
+  email: string;
+  password: string;
+  submit: null;
+}
+
+const initialValues: Values = {
+  email: "muzzammil@talentscan.com",
+  password: "muzzammil@talent",
+  submit: null,
+};
+
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .email("Must be a valid email")
+    .max(255)
+    .required("Email is required"),
+  password: Yup.string().max(255).required("Password is required"),
+});
+
+const Page = () => {
+  const isMounted = useMounted();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const returnTo = searchParams.get("returnTo");
+  const { signIn } = useAuth<AuthContextType>();
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: async (values, helpers): Promise<void> => {
+      try {
+        await signIn(values.email, values.password);
+
+        if (isMounted()) {
+          router.push(returnTo || paths.dashboard.index);
+        }
+      } catch (err) {
+        console.error(err);
+
+        if (isMounted()) {
+          helpers.setStatus({ success: false });
+          helpers.setErrors({ submit: err.message });
+          helpers.setSubmitting(false);
+        }
+      }
+    },
+  });
+
+  return (
+    <>
+      <Seo title="Login" />
+      <div>
+        <Box sx={{ mb: 4 }}>
+          <Link
+            color="text.primary"
+            component={RouterLink}
+            href={paths.dashboard.index}
+            sx={{
+              alignItems: "center",
+              display: "inline-flex",
+            }}
+            underline="hover"
+          >
+            <SvgIcon sx={{ mr: 1 }}>
+              <ArrowLeftIcon />
+            </SvgIcon>
+            <Typography variant="subtitle2">Dashboard</Typography>
+          </Link>
+        </Box>
+        <Stack sx={{ mb: 4 }} spacing={1}>
+          <Typography variant="h5">Log in</Typography>
+          <Typography color="text.secondary" variant="body2">
+            Don&apos;t have an account? &nbsp;
+            <Link href="#" underline="hover" variant="subtitle2">
+              Register
+            </Link>
+          </Typography>
+        </Stack>
+        <form noValidate onSubmit={formik.handleSubmit}>
+          <Stack spacing={3}>
+            <TextField
+              autoFocus
+              error={!!(formik.touched.email && formik.errors.email)}
+              fullWidth
+              helperText={formik.touched.email && formik.errors.email}
+              label="Email Address"
+              name="email"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="email"
+              value={formik.values.email}
+            />
+            <TextField
+              error={!!(formik.touched.password && formik.errors.password)}
+              fullWidth
+              helperText={formik.touched.password && formik.errors.password}
+              label="Password"
+              name="password"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="password"
+              value={formik.values.password}
+            />
+          </Stack>
+          {formik.errors.submit && (
+            <FormHelperText error sx={{ mt: 3 }}>
+              {formik.errors.submit as string}
+            </FormHelperText>
+          )}
+          <Button
+            fullWidth
+            sx={{ mt: 3 }}
+            size="large"
+            type="submit"
+            variant="contained"
+          >
+            Continue
+          </Button>
+          <Box sx={{ mt: 3 }}>
+            <Link href="#" underline="hover" variant="subtitle2">
+              Forgot password?
+            </Link>
+          </Box>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Page;
