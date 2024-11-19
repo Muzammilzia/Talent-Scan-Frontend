969d29d8a83d8662d1ee9b55fc1e110f2c509f7a
muniba <111457423+MunibaGhayas@users.noreply.github.com>
2024-11-19 23:35:48 +0500

    ...

--- src/pages/auth/company/login.tsx
+++ src/pages/auth/company/login.tsx
@@ -1,17 +1,25 @@
-import * as Yup from 'yup';
-import { useFormik } from 'formik';
-import ArrowLeftIcon from '@untitled-ui/icons-react/build/esm/ArrowLeft';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
-import Stack from '@mui/material/Stack';
-import SvgIcon from '@mui/material/SvgIcon';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
 
-import { RouterLink } from 'src/components/router-link';
-import { Seo } from 'src/components/seo';
-import { paths } from 'src/paths';
+// IN USE, TEMPLATE FROM pages/auth-demo/login/modern.tsx
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import ArrowLeftIcon from "@untitled-ui/icons-react/build/esm/ArrowLeft";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import SvgIcon from "@mui/material/SvgIcon";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+
+import { RouterLink } from "src/components/router-link";
+import { Seo } from "src/components/seo";
+import { paths } from "src/paths";
+import { AuthContextType } from "src/contexts/auth/jwt-context";
+import { useAuth } from "src/hooks/use-auth";
+import { useMounted } from "src/hooks/use-mounted";
+import { useRouter } from "src/hooks/use-router";
+import { useSearchParams } from "src/hooks/use-search-params";
+import { FormHelperText } from "@mui/material";
 
 interface Values {
   email: string;
@@ -20,28 +28,45 @@ interface Values {
 }
 
 const initialValues: Values = {
-  email: '',
-  password: '',
-  submit: null
+  email: "",
+  password: "",
+  submit: null,
 };
 
 const validationSchema = Yup.object({
-  email: Yup
-    .string()
-    .email('Must be a valid email')
+  email: Yup.string()
+    .email("Must be a valid email")
     .max(255)
-    .required('Email is required'),
-  password: Yup
-    .string()
-    .max(255)
-    .required('Password is required')
+    .required("Email is required"),
+  password: Yup.string().max(255).required("Password is required"),
 });
 
 const Page = () => {
+  const isMounted = useMounted();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const returnTo = searchParams.get("returnTo");
+  const { signIn } = useAuth<AuthContextType>();
   const formik = useFormik({
     initialValues,
     validationSchema,
-    onSubmit: (): void => {}
+    onSubmit: async (values, helpers): Promise<void> => {
+      try {
+        await signIn(values.email, values.password);
+
+        if (isMounted()) {
+          router.push(returnTo || paths.dashboard.index);
+        }
+      } catch (err) {
+        console.error(err);
+
+        if (isMounted()) {
+          helpers.setStatus({ success: false });
+          helpers.setErrors({ submit: err.message });
+          helpers.setSubmitting(false);
+        }
+      }
+    },
   });
 
   return (
@@ -54,47 +79,27 @@ const Page = () => {
             component={RouterLink}
             href={paths.dashboard.index}
             sx={{
-              alignItems: 'center',
-              display: 'inline-flex'
+              alignItems: "center",
+              display: "inline-flex",
             }}
             underline="hover"
           >
             <SvgIcon sx={{ mr: 1 }}>
               <ArrowLeftIcon />
             </SvgIcon>
-            <Typography variant="subtitle2">
-              Dashboard
-            </Typography>
+            <Typography variant="subtitle2">Dashboard</Typography>
           </Link>
         </Box>
-        <Stack
-          sx={{ mb: 4 }}
-          spacing={1}
-        >
-          <Typography
-            variant="h5"
-          >
-            Log in
-          </Typography>
-          <Typography
-            color="text.secondary"
-            variant="body2"
-          >
-            Don&apos;t have an account?
-            &nbsp;
-            <Link
-              href="#"
-              underline="hover"
-              variant="subtitle2"
-            >
+        <Stack sx={{ mb: 4 }} spacing={1}>
+          <Typography variant="h5">Log in</Typography>
+          <Typography color="text.secondary" variant="body2">
+            Don&apos;t have an account? &nbsp;
+            <Link href="#" underline="hover" variant="subtitle2">
               Register
             </Link>
           </Typography>
         </Stack>
-        <form
-          noValidate
-          onSubmit={formik.handleSubmit}
-        >
+        <form noValidate onSubmit={formik.handleSubmit}>
           <Stack spacing={3}>
             <TextField
               autoFocus
@@ -120,6 +125,11 @@ const Page = () => {
               value={formik.values.password}
             />
           </Stack>
+          {formik.errors.submit && (
+            <FormHelperText error sx={{ mt: 3 }}>
+              {formik.errors.submit as string}
+            </FormHelperText>
+          )}
           <Button
             fullWidth
             sx={{ mt: 3 }}
@@ -130,11 +140,7 @@ const Page = () => {
             Continue
           </Button>
           <Box sx={{ mt: 3 }}>
-            <Link
-              href="#"
-              underline="hover"
-              variant="subtitle2"
-            >
+            <Link href="#" underline="hover" variant="subtitle2">
               Forgot password?
             </Link>
           </Box>
